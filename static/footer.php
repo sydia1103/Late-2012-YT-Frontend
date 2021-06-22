d7151d7a301ce314ba1baee043a7fa0874c2bb09
Taniko <83744038+Valmoiiaa@users.noreply.github.com>
2021-06-22 22:10:49 +0900

    Add files via upload

---
+++ static/footer.php
@@ -0,0 +1,92 @@
+<div id="footer-hh-container">    <div id="footer-hh"><div id="footer-hh-main"><div id="footer-hh-logo"><a href="/" title="YouTube home"><img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt="YouTube home"></a></div>  <ul class="pickers yt-uix-button-group" data-button-toggle-group="optional">
+      <li>
+          
+  <button id="yt-picker-language-button" type="button" class=" yt-uix-button yt-uix-button-hh-default" onclick=";return false;" data-button-menu-id="arrow-display" data-button-toggle="true" data-picker-key="language" data-button-action="yt.www.picker.load" data-picker-position="footer" role="button"><span class="yt-uix-button-content">    <span class="yt-picker-button-label">
+Language:
+    </span>
+  English
+ </span><img class="yt-uix-button-arrow" src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt=""></button>
+
+
+      </li>
+      <li>
+          
+  <button id="yt-picker-language-button" type="button" class=" yt-uix-button yt-uix-button-hh-default" onclick=";return false;" data-button-menu-id="arrow-display" data-button-toggle="true" data-picker-key="country" data-button-action="yt.www.picker.load" data-picker-position="footer" role="button"><span class="yt-uix-button-content">    <span class="yt-picker-button-label">
+Country:
+    </span>
+  Worldwide
+ </span><img class="yt-uix-button-arrow" src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt=""></button>
+
+
+      </li>
+      <li>
+          
+  <button id="yt-picker-language-button" type="button" class=" yt-uix-button yt-uix-button-hh-default" onclick=";return false;" data-button-menu-id="arrow-display" data-button-toggle="true" data-picker-key="safetymode" data-button-action="yt.www.picker.load" data-picker-position="footer" role="button"><span class="yt-uix-button-content">    <span class="yt-picker-button-label">
+Safety:
+    </span>
+Off
+ </span><img class="yt-uix-button-arrow" src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" alt=""></button>
+
+
+      </li>
+  </ul>
+
+    <div id="in-product-help" class="yt-uix-clickcard">
+      <button id="help-button" type="button" class="yt-uix-clickcard-target yt-uix-button-reverse yt-uix-button yt-uix-button-hh-default" onclick=";return false;" data-locale="en_US" data-position="bottomleft" data-iph-js-url="//s.ytimg.com/yts/jsbin/www-help-vflDI78xO.js" data-iph-title-text="Need Help on this page?" data-iph-css-url="//s.ytimg.com/yts/cssbin/www-helpie-vflVn-ywY.css" data-iph-search-button-text="Search" data-orientation="vertical" data-iph-anchor-text="More Help" data-iph-search-input-text="Search YouTube's Help Center" data-iph-topic-id="1699306" data-help-center-host="//support.google.com/youtube" data-iph-tracking="iph-questionmark" role="button"><span class="yt-uix-button-content">  <img class="questionmark" src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif">
+  <span>Help</span>
+  <img class="yt-uix-button-arrow" src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif">
+ </span></button>
+      <div class="yt-uix-clickcard-content" id="help-target">  <p class="yt-spinner">
+      <img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="Loading icon">
+
+Loading...
+  </p>
+</div>
+    </div>
+      <div id="yt-picker-language-footer" class="yt-picker" style="display: none">
+      <p class="yt-spinner">
+      <img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="Loading icon">
+
+Loading...
+  </p>
+
+  </div>
+
+      <div id="yt-picker-country-footer" class="yt-picker" style="display: none">
+      <p class="yt-spinner">
+      <img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="Loading icon">
+
+Loading...
+  </p>
+
+  </div>
+
+      <div id="yt-picker-safetymode-footer" class="yt-picker" style="display: none">
+      <p class="yt-spinner">
+      <img src="//s.ytimg.com/yts/img/pixel-vfl3z5WfW.gif" class="yt-spinner-img" alt="Loading icon">
+
+Loading...
+  </p>
+
+  </div>
+
+</div><div id="footer-hh-links"><ul id="footer-hh-links-primary">  <li><a href="/t/about_youtube">About</a></li>
+  <li><a href="/t/press">Press &amp; Blogs</a></li>
+  <li><a href="/t/copyright_center">Copyright</a></li>
+  <li><a href="/creators">Creators &amp; Partners</a></li>
+  <li><a href="/t/advertising_overview">Advertising</a></li>
+  <li><a href="/dev">Developers</a></li>
+</ul><ul id="footer-hh-links-secondary">  <li><a href="/t/terms">Terms</a></li>
+  <li><a href="http://www.google.com/intl/en/policies/privacy/">Privacy</a></li>
+  <li><a href="//support.google.com/youtube/bin/request.py?contact_type=abuse&amp;hl=en-US">Safety</a></li>
+
+  <li><a href="//www.google.com/tools/feedback/intl/en/error.html" onclick="return yt.www.feedback.start(yt.getConfig('FEEDBACK_LOCALE_LANGUAGE'), yt.getConfig('FEEDBACK_LOCALE_EXTRAS'), null, yt.getConfig('FEEDBACK_BUCKET_ID'));" id="reportbug">Send feedback</a></li>
+  <li><a href="/testtube">Try something new!</a></li>
+
+    <li>  <span class="copyright" dir="ltr">© 2013 YouTube, LLC</span>
+</li>
+
+</ul></div>        
+
+</div>
+</div>
